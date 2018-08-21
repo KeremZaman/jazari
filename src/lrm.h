6a022cb69fae2dc7cf7d67051ae9b3a06cbfccf5
Kerem Zaman <KeremZaman@users.noreply.github.com>
2018-08-22 02:18:46 +0300

    Added Logistic Regression model.

--- src/lrm.h
+++ src/lrm.h
@@ -4,14 +4,18 @@ struct lrm {
 	Matrix* params;
 	Matrix* tr_data;
 	Matrix* targets;
-	int epoch;
+	int epoch_num;
 	double lr; //learning rate
 	void(*run) (struct lrm*); //function pointer to run
 	double(*cost_f) (struct lrm*);
-	double(*hyp) (void); //hyphothesis
+	double(*hyp) (Matrix* params, Matrix* features); //hyphothesis
 };
-typedef struct lrm LinRegModel;
+typedef struct lrm LRModel;
 
-double hyp(Matrix* params, Matrix* features);
-void sgd(LinRegModel* lrm);
-LinRegModel* init_lrm(Matrix* params, Matrix* tr_data, Matrix* targets, double lr, int epoch);
+double lin_hyp(Matrix* params, Matrix* features);
+double log_hyp(Matrix* params, Matrix* features);
+double linRM_cost(LRModel* lrm);
+double logRM_cost(LRModel* lrm);
+void sgd(LRModel* lrm);
+LRModel* init_linRM(Matrix* params, Matrix* tr_data, Matrix* targets, double lr, int epoch_num);
+LRModel* init_logRM(Matrix* params, Matrix* tr_data, Matrix* targets, double lr, int epoch_num);
